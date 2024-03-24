43157bfcd0acf332655be3cdc9abc826f2d1c4d1
sadia saadi <saadiasaadi944@gmail.com>
2024-03-24 05:39:50 +0500

    2files

---
+++ app.js
@@ -0,0 +1,108 @@
+// chapter 21 (changimg case)
+
+
+// q1
+
+// var allLower = userInput.toLowerCase();
+
+// q2
+
+var x = "X";
+console.log(x.toLowerCase());
+
+// Q3
+
+var y = "y";
+console.log(y.toUpperCase());
+
+// q4
+
+var t = "T";
+var u = t.toLowerCase();
+console.log(u);
+
+// Q5
+
+var arr = ["BAX", "CDDS","FDS", "CJCJZ"];
+var str2 = arr[3].toLowerCase();
+console.log(str2);
+
+// Q6
+
+var cvg = "sadiasaaadi";
+alert(cvg.toUpperCase());
+
+// Q7
+
+// var cityName = "kaRacHi";
+// var cityName = CharAt(0).toUpperCase() +cityName
+//  var a = firstChar.toUpperCase();
+//  console.log(a);
+
+
+// CHAPTER NO 22 (STRINGS)
+
+// Q1
+
+var sameWords = "captain";
+var sliceWords = sameWords.slice(1,3);
+console.log(sliceWords);
+
+// Q2
+
+var num = "sadia saadi";
+var lengthOfStr = num.length;
+console.log(lengthOfStr);
+
+// Q3
+
+var animal = "elephant";
+var seg = animal.substr(2,4);
+console.log(seg);
+
+// Q4
+
+var animal = "elephant";
+var segLength = animal.length;
+console.log(segLength);
+
+// Q5
+
+var number = "sadia saadi";
+var strLength = number.length;
+var sliceStr = number.slice(1,-3);
+console.log(sliceStr);
+
+// Q6
+
+var text = "to be or not to be";
+var indx = text.lastIndexOf("be");
+console.log(indx);
+
+// Q7
+
+var text = "to be or not to be";
+var indx = text.indexOf("be");
+console.log(indx);
+
+// Q8
+
+var text = "go";
+var textstr = text.indexOf("go");
+console.log(textstr);
+
+// Q9
+
+var str = 56;
+var indexNum = 34;
+if (str.length > indexNum && indexNum >= 0) {
+    console.log("yes");
+}
+
+// Q10
+
+var a = "abcde";
+var str2 = a.charAt(2);
+console.log(str2);
+// Q13
+
